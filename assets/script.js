208e11cbfee5c8ae96ff069ca0c2df519968b6c8
paulina-pacheco-o <paulimichi05@gmail.com>
2025-07-17 16:37:18 +0200

    cards js

--- assets/script.js
+++ assets/script.js
@@ -36,3 +36,30 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+const teamContainer = document.getElementById(`teamContainer`);
+
+let cards = ``
+
+for (let i = 0; i < teamMembers.length; i++) {
+
+  const member = teamMembers[i];
+  const { name, role, email, img } = member;
+
+  cards += `<div class="col-4 mt-5 ml-3 mb-5">
+          <div class="card bg-dark border-0">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img src="${img}">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title text-white">${name}</h5>
+                  <p class="card-text text-white">${role}</p>
+                  <p class="card-text"><small class="text-primary">${email}</small></p>
+                </div>
+              </div>
+            </div>
+          </div>`
+}
+
